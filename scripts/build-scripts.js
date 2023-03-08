2acb2988a8c32b70dd5e42c9688a13d595e0cd19
MihawK <127223292+Burhanverse@users.noreply.github.com>
2023-03-08 14:37:55 +0530

    Delete scripts directory

--- scripts/build-scripts.js
+++
@@ -1,6 +0,0 @@
-
-'use strict';
-
-const renderScripts = require('./render-scripts');
-
-renderScripts();
--- scripts/build-scss.js
+++
@@ -1,6 +0,0 @@
-
-'use strict';
-
-const renderSCSS = require('./render-scss');
-
-renderSCSS();
